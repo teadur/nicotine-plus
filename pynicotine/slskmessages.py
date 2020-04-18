1e2a3d4405b87c0953834310d5e122d20f51a59e
Lene Preuss <lene.preuss@here.com>
2020-04-18 22:29:38 +0200

    Downloading folders works thanks to eliminating another obsolete ord()

--- pynicotine/slskmessages.py
+++ pynicotine/slskmessages.py
@@ -1930,7 +1930,7 @@ class FolderContentsResponse(PeerMessage):
                 pos, nfiles = self.getObject(message, int, pos)
                 shares[folder][directory] = []
                 for j in range(nfiles):
-                    pos, code = pos + 1, ord(message[pos])
+                    pos, code = pos + 1, message[pos]
                     pos, name = self.getObject(message, bytes, pos, printerror=False)
                     pos, size = self.getObject(message, sizetype, pos, getsignedint=1, printerror=False)
                     pos, ext = self.getObject(message, bytes, pos, printerror=False)
